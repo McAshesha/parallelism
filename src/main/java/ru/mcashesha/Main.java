9852805fb4aa9145d4b5cd10fecd3344383af6fb
Vsevolod Kovalev <mcashesha@mail.ru>
2026-02-05 17:09:54 +0700

    init

---
+++ src/main/java/ru/mcashesha/Main.java
@@ -0,0 +1,33 @@
+package ru.mcashesha;
+
+import java.util.stream.IntStream;
+
+public class Main {
+
+    static final int N = 10_000_000;
+
+    static final boolean USE_FLOAT = System.getProperty("arrayType", "double").equalsIgnoreCase("float");
+
+    static void main(String[] args) {
+
+        double sum, step = 2.0 * Math.PI / (N -1);
+
+        if (USE_FLOAT) {
+            sum = IntStream.range(0, N)
+                .parallel()
+                .mapToDouble(i -> (float) Math.sin(step * i))
+                .sum();
+
+        } else {
+            sum = IntStream.range(0, N)
+                .parallel()
+                .mapToDouble(i -> Math.sin(step * i))
+                .sum();
+        }
+
+
+        System.out.printf("Type: %s%n", USE_FLOAT ? "float" : "double");
+        System.out.printf("N: %d%n", N);
+        System.out.printf("Sum: %.17g%n", sum);
+    }
+}
